ad6765f00baa1ebbc49350743304846e0de604cd
Wander S. Moschetta <wander.sm@gmail.com>
2025-07-17 23:31:06 -0300

    Feat: first part of the layout stack

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -9,13 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [.blue, .white]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing)
+            .edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                Text("Sao Jose do Rio Preto")
+                    .font(.system(size: 32, weight: .medium,design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                VStack(spacing: 10){
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    Text("25°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+            
+                }
+                HStack{
+                    VStack{
+                        Text("Ter.")
+                            .font(.system(size: 16, weight: .medium, design: .default))
+                            .foregroundColor(.white)
+                        Image(systemName: "cloud.sun.fill")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 50, height: 50)
+                        Text("24°")
+                            .font(.system(size: 32, weight: .medium))
+                            .foregroundColor(.white)
+                                         
+                    }
+                }
+                Spacer()
+            }
         }
-        .padding()
+        
     }
 }
 
