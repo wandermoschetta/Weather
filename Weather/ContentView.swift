9aca569005c2641dc45586270290ed8cfe7a068c
Wander S. Moschetta <wander.sm@gmail.com>
2025-07-26 00:05:32 -0300

    feat: weatherdayweek view

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -32,26 +32,66 @@ struct ContentView: View {
                         .foregroundColor(.white)
             
                 }
-                HStack{
-                    VStack{
-                        Text("Ter.")
-                            .font(.system(size: 16, weight: .medium, design: .default))
-                            .foregroundColor(.white)
-                        Image(systemName: "cloud.sun.fill")
-                            .renderingMode(.original)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 50, height: 50)
-                        Text("24°")
-                            .font(.system(size: 32, weight: .medium))
-                            .foregroundColor(.white)
-                                         
-                    }
+                HStack(spacing: 10){
+                    WeatherDayView(dayOfWeek: "Terc.",
+                                   imageName: "cloud.sun.fill",
+                                   maxTemperature:"24",
+                                   minTemperature: "15")
+         
+                    WeatherDayView(dayOfWeek: "Quar.",
+                                   imageName: "cloud.sun.fill",
+                                   maxTemperature: "25",
+                                   minTemperature: "16")
+         
                 }
                 Spacer()
             }
         }
         
+    }
+}
+
+struct WeatherDayView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var maxTemperature: Int
+    var minTemperature: Int
+    
+    
+    var body: some View {
+       VStack{
+           Text(dayOfWeek)
+               .font(.system(size: 16, weight: .medium, design: .default))
+               .foregroundColor(.white)
+               
+           Image(systemName: imageName)
+               .renderingMode(.original)
+               .resizable()
+               .aspectRatio(contentMode: .fit)
+               .frame(width: 50, height: 50)
+           
+           HStack{	
+               Text("Max.")
+                   .font(.system(size: 16, weight: .medium))
+                   .foregroundColor(.white)
+               
+               Text("\(maxTemperature)°")
+                   .font(.system(size: 32, weight: .medium))
+                   .foregroundColor(.white)
+           }
+           
+           HStack(){
+               Text("Min.")
+                   .font(.system(size: 16, weight: .medium))
+                   .foregroundColor(.white)
+               
+               Text("\(minTemperature)°")
+                   .font(.system(size: 32, weight: .medium))
+                   .foregroundColor(.white)
+           }
+       }
+       
     }
 }
 
