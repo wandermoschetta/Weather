f0e1a0165bb0b9ba36439c801b4198c7892ce3c4
Wander S. Moschetta <wander.sm@gmail.com>
2025-07-29 23:47:53 -0300

    feat: refact slices of the file to some views components

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -7,43 +7,52 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(
-                gradient: Gradient(colors: [.blue, .white]),
-                startPoint: .topLeading,
-                endPoint: .bottomTrailing)
-            .edgesIgnoringSafeArea(.all)
+            BackgroundView(topColor: .blue, bottomColor: Color("lightBlue"))
             
             VStack{
-                Text("Sao Jose do Rio Preto")
-                    .font(.system(size: 32, weight: .medium,design: .default))
-                    .foregroundColor(.white)
-                    .padding()
-                VStack(spacing: 10){
-                    Image(systemName: "cloud.sun.fill")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 180, height: 180)
-                    Text("25°")
-                        .font(.system(size: 70, weight: .medium))
-                        .foregroundColor(.white)
-            
-                }
+                CityTextView(city: "São José do Rio Preto")
+                
+                MainWeatherStatusView(temperature: 25,
+                                      imageName: "cloud.sun.fill")
+                
                 HStack(spacing: 10){
-                    WeatherDayView(dayOfWeek: "Terc.",
+                    WeatherDayView(dayOfWeek: "Ter.",
                                    imageName: "cloud.sun.fill",
                                    maxTemperature:24,
                                    minTemperature: 15)
          
-                    WeatherDayView(dayOfWeek: "Quar.",
+                    WeatherDayView(dayOfWeek: "Qua.",
+                                   imageName: "sun.max.fill",
+                                   maxTemperature: 30,
+                                   minTemperature: 19)
+                    
+                    WeatherDayView(dayOfWeek: "Qui.",
                                    imageName: "cloud.sun.fill",
-                                   maxTemperature: 25,
-                                   minTemperature: 16)
+                                   maxTemperature: 26,
+                                   minTemperature: 15)
+                    
+                    WeatherDayView(dayOfWeek: "Qui.",
+                                   imageName: "cloud.sun.fill",
+                                   maxTemperature: 26,
+                                   minTemperature: 15)
          
                 }
+                Spacer()
+                
+                Button{
+                    print("tapped...")
+                } label: {
+                    WeatherButton(title: "Change Day Time",
+                                      textColor: .blue,
+                                      backgroundColor: .white)
+                                      
+                    
+                }
+                
                 Spacer()
             }
         }
@@ -69,31 +78,78 @@ struct WeatherDayView: View {
                .renderingMode(.original)
                .resizable()
                .aspectRatio(contentMode: .fit)
-               .frame(width: 50, height: 50)
+               .frame(width: 50, height: 40)
            
-           HStack{	
+           HStack(spacing: 5){
                Text("Max.")
-                   .font(.system(size: 16, weight: .medium))
+                   .font(.system(size: 14, weight: .medium))
                    .foregroundColor(.white)
                
                Text("\(maxTemperature)°")
-                   .font(.system(size: 32, weight: .medium))
+                   .font(.system(size: 28, weight: .medium))
                    .foregroundColor(.white)
            }
            
-           HStack(){
+           HStack(spacing: 5){
                Text("Min.")
-                   .font(.system(size: 16, weight: .medium))
+                   .font(.system(size: 14, weight: .medium))
                    .foregroundColor(.white)
                
                Text("\(minTemperature)°")
-                   .font(.system(size: 32, weight: .medium))
+                   .font(.system(size: 28, weight: .medium))
                    .foregroundColor(.white)
            }
+           
        }
        
     }
 }
+
+struct BackgroundView: View {
+    var topColor: Color
+    var bottomColor: Color
+    
+    var body: some View {
+        LinearGradient(
+            gradient: Gradient(colors: [topColor, bottomColor]),
+            startPoint: .topLeading,
+            endPoint: .bottomTrailing)
+        .edgesIgnoringSafeArea(.all)
+        
+    }
+}
+
+struct CityTextView: View {
+    var city: String
+    
+    var body: some View {
+        Text("\(city)")
+            .font(.system(size: 32, weight: .medium,design: .default))
+            .foregroundColor(.white)
+            .padding()
+    }
+}
+
+struct MainWeatherStatusView: View {
+    var temperature: Int
+    var imageName: String
+    
+    var body: some View {
+    
+        VStack(spacing: 10){
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180, height: 180)
+            Text("\(temperature)°")
+                .font(.system(size: 70, weight: .medium))
+                .foregroundColor(.white)
+    
+        }
+    }
+}
+
 
 #Preview {
     ContentView()
